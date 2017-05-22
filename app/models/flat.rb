ffd79380cb0ccb0b92e38071f139331145708375
clement berger <clement.berger@gmail.com>
2017-05-22 15:09:49 +0200

    user & flat model with validates rules

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,13 @@
 class Flat < ApplicationRecord
   belongs_to :users
+
+      validates :title, presence:true
+      validates :description
+      validates :area, presence:true, numericality: true
+      validates :address
+      validates :zip, presence:true, format: { with: '/^((0[1-9])|([1-8][0-9])|(9[0-8]|(2A)|(2B))[0-9]{3})$/', message: "only french zip code" }
+      validates :price, presence:true
+      validates :availability
+      validates :rooms, presence:true, numericality: {other_than: 0, only_integer: true}
+      validates :type, presence:true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :flats
+
+  validates :first_name, presence:true
+  validates :first_name, presence:true
+
 end
