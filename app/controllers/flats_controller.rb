117b9538758ab74249d475f61bd654ad830211f4
clement berger <clement.berger@gmail.com>
2017-05-22 17:36:34 +0200

    correction of user from flats

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,7 +1,32 @@
 class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(params_flat)
+    @flat.user = current_user
+    binding.pry
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
   end
+
+
+  private
+    def params_flat
+      params.require(:flat).permit(:title, :description, :area, :address, :zip, :price, :availability, :rooms)
+    end
 end
+
+
+
+
