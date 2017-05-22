32b3fab1b9b5c3d10dc3fa81d04f52fde21d494f
clement berger <clement.berger@gmail.com>
2017-05-22 19:42:36 +0200

    index users controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :flats, only: [:index, :new, :create, :edit, :update, :destroy, :show, :index]
+  resources :users, only:[:index, :show]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
