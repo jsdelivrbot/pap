1340f8aa86405dff376cbc364633a80f567ad63d
matthieu reboul <matthieu.reboul@gmail.com>
2017-05-23 15:30:35 +0200

    correct profile route and add redirection to profile after sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
 
   resources :flats, only: [:index, :new, :create, :edit, :update, :destroy, :show, :index]
-  resources :users, only:[:index, :show]
+  resources :users, only:[:show]
 
+  resource :profile, only: :show
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
