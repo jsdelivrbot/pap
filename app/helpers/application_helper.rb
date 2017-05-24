bd67c1c258a0ee43545d491225c6097dbb1fea96
clement berger <clement.berger@gmail.com>
2017-05-24 14:49:34 +0200

    form display done-first v

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def btn_facebook
+     resource_class.omniauth_providers.each do |provider|
+       return link_to "Sign in with #{OmniAuth::Utils.camelize(provider)}",
+      omniauth_authorize_path(resource_name, provider),
+      :class=>"navbar-wagon-item navbar-wagon-button btn"
+   end
+  end
+
+  def forgot_password
+    link_to "Forgot your password?", new_password_path(resource_name), :class=>"navbar-wagon-item navbar-wagon-button btn"
+  end
+
+  def sign_up
+    link_to "Sign up", new_registration_path(resource_name), :class=>"navbar-wagon-item navbar-wagon-button btn"
+  end
+
 end
