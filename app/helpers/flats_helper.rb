17f06d7ef22f51145a94fd71819ae23bd3f6df06
matthieu reboul <matthieu.reboul@gmail.com>
2017-05-24 16:00:41 +0200

    add infos and pictures in flats#show view

--- app/helpers/flats_helper.rb
+++ app/helpers/flats_helper.rb
@@ -1,11 +1,7 @@
 module FlatsHelper
   def photo_path_for(flat)
     if flat.photos?
-      cl_image_path flat.photos[0].path
-    elsif cl_image_path "hsrta9zuz8sff8u3jbfv".present?
-      cl_image_path "hsrta9zuz8sff8u3jbfv"
-    else
-      ''
+      cl_image_path flat.photos.first.path
     end
   end
 end
