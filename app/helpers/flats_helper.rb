d6cfbe51baade967f33728cdfadb3b09b0a516bc
clement berger <clement.berger@gmail.com>
2017-05-24 10:21:11 +0200

    update nav_bar_mobile path

--- app/helpers/flats_helper.rb
+++ app/helpers/flats_helper.rb
@@ -2,6 +2,12 @@ module FlatsHelper
   def photo_path_for(flat)
     if flat.photo?
       cl_image_path flat.photo.path
+    elsif cl_image_path "hsrta9zuz8sff8u3jbfv".present?
+      cl_image_path "hsrta9zuz8sff8u3jbfv"
+    else
+      ''
     end
   end
 end
+
+
